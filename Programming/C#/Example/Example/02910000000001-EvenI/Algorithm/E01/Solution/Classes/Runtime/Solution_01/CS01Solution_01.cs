bddab9bbfb79b117af52a3d1f77572e3bd311289
lkstudio.dante <lkstudio.dante@gmail.com>
2025-05-03 01:12:48 +0900

    no message

---
+++ Programming/C#/Example/Example/02910000000001-EvenI/Algorithm/E01/Solution/Classes/Runtime/Solution_01/CS01Solution_01.cs
@@ -0,0 +1,97 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example._02910000000001_EvenI.Algorithm.E01.Solution.Classes.Runtime.Solution_01
+{
+	/**
+	 * Solution 1
+	 */
+	internal class CS01Solution_01
+	{
+		/** 초기화 */
+		public static void Start(string[] args)
+		{
+			var oRandom = new Random();
+			var oListValues = new List<int>();
+
+			for(int i = 0; i < 10; ++i)
+			{
+				oListValues.Add(oRandom.Next(1, 100));
+			}
+
+			S01SortValues_01(oListValues);
+
+			Console.WriteLine("=====> 리스트 요소 <=====");
+			S01PrintValues_01(oListValues);
+
+			Console.Write("\n정수 입력 : ");
+			int.TryParse(Console.ReadLine(), out int nVal);
+
+			int nResult = S01FindVal_01(oListValues, nVal);
+			Console.WriteLine("결과 : {0}", nResult);
+		}
+
+		/** 값을 탐색한다 */
+		private static int S01FindVal_01(List<int> a_oListValues, int a_nVal)
+		{
+			return S01FindVal_Internal_01(a_oListValues, 
+				a_nVal, 0, a_oListValues.Count - 1);
+		}
+
+		/** 값을 탐색한다 */
+		private static int S01FindVal_Internal_01(List<int> a_oListValues,
+			int a_nVal, int a_nLeft, int a_nRight)
+		{
+			// 값 탐색이 불가능 할 경우
+			if(a_nLeft > a_nRight)
+			{
+				return -1;
+			}
+
+			int nMiddle = (a_nLeft + a_nRight) / 2;
+
+			// 값이 존재 할 경우
+			if(a_nVal == a_oListValues[nMiddle])
+			{
+				return nMiddle;
+			}
+
+			return (a_nVal < a_oListValues[nMiddle]) ?
+				S01FindVal_Internal_01(a_oListValues, a_nVal, a_nLeft, nMiddle - 1) : S01FindVal_Internal_01(a_oListValues, a_nVal, nMiddle + 1, a_nRight);
+		}
+
+		/** 값을 정렬한다 */
+		private static void S01SortValues_01(List<int> a_oListValues)
+		{
+			for(int i = 0; i < a_oListValues.Count - 1; ++i)
+			{
+				for(int j = 0; j < a_oListValues.Count - (i + 1); ++j)
+				{
+					// 정렬이 필요없을 경우
+					if(a_oListValues[j] < a_oListValues[j + 1])
+					{
+						continue;
+					}
+
+					int nTemp = a_oListValues[j];
+					a_oListValues[j] = a_oListValues[j + 1];
+					a_oListValues[j + 1] = nTemp;
+				}
+			}
+		}
+
+		/** 값을 출력한다 */
+		private static void S01PrintValues_01(List<int> a_oListValues)
+		{
+			for(int i = 0; i < a_oListValues.Count; ++i)
+			{
+				Console.Write("{0}, ", a_oListValues[i]);
+			}
+
+			Console.WriteLine();
+		}
+	}
+}
---
+++ Programming/C#/Example/Example/02910000000001-EvenI/Algorithm/E01/Solution/Classes/Runtime/Solution_02/CS01Solution_02.cs
@@ -0,0 +1,130 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example._02910000000001_EvenI.Algorithm.E01.Solution.Classes.Runtime.Solution_02
+{
+	/**
+	 * Solution 2
+	 */
+	internal class CS01Solution_02
+	{
+		/** 초기화 */
+		public static void Start(string[] args)
+		{
+			var oRandom = new Random();
+			var oListValues = new List<int>();
+
+			for(int i = 0; i < 10; ++i)
+			{
+				oListValues.Add(oRandom.Next(1, 100));
+			}
+
+			Console.WriteLine("=====> 리스트 요소 - 정렬 전 <=====");
+			S01PrintValues_02(oListValues);
+
+			S01SortValues_02(oListValues, S01Compare_ByAscending_02);
+
+			Console.WriteLine("\n=====> 리스트 요소 - 정렬 후 (오름차순) <=====");
+			S01PrintValues_02(oListValues);
+
+			S01SortValues_02(oListValues, S01Compare_ByDescending_02);
+
+			Console.WriteLine("\n=====> 리스트 요소 - 정렬 후 (내림차순) <=====");
+			S01PrintValues_02(oListValues);
+		}
+
+		/** 오름차순으로 비교한다 */
+		private static int S01Compare_ByAscending_02(int a_nLhs, int a_nRhs)
+		{
+			return a_nLhs - a_nRhs;
+		}
+
+		/** 내림차순으로 비교한다 */
+		private static int S01Compare_ByDescending_02(int a_nLhs, int a_nRhs)
+		{
+			return a_nRhs - a_nLhs;
+		}
+
+		/** 값을 정렬한다 */
+		private static void S01SortQuick_02(List<int> a_oListValues,
+			int a_nLeft, int a_nRight, Func<int, int, int> a_oCompare)
+		{
+			// 정렬이 불가능 할 경우
+			if(a_nLeft >= a_nRight)
+			{
+				return;
+			}
+
+			int nMiddle = (a_nLeft + a_nRight) / 2;
+
+			int nIdx_Pivot = (a_oCompare(a_oListValues[a_nLeft], a_oListValues[nMiddle]) >= 0) ? 
+				a_nLeft : nMiddle;
+
+			nIdx_Pivot = (a_oCompare(a_oListValues[nIdx_Pivot], a_oListValues[a_nRight]) <= 0) ?
+				nIdx_Pivot : a_nRight;
+
+			int nTemp = a_oListValues[a_nLeft];
+			a_oListValues[a_nLeft] = a_oListValues[nIdx_Pivot];
+			a_oListValues[nIdx_Pivot] = nTemp;
+
+			int nLeft = a_nLeft + 1;
+			int nRight = a_nRight;
+			int nPivot = a_nLeft;
+
+			while(true)
+			{
+				while(nLeft < nRight && a_oCompare(a_oListValues[nPivot], a_oListValues[nLeft]) >= 0)
+				{
+					nLeft += 1;
+				}
+
+				while(nLeft <= nRight && a_oCompare(a_oListValues[nPivot], a_oListValues[nRight]) <= 0)
+				{
+					nRight -= 1;
+				}
+
+				// 정렬이 불가능 할 경우
+				if(nLeft >= nRight)
+				{
+					break;
+				}
+
+				nTemp = a_oListValues[nLeft];
+				a_oListValues[nLeft] = a_oListValues[nRight];
+				a_oListValues[nRight] = nTemp;
+			}
+
+			// 정렬이 필요 할 경우
+			if(a_oCompare(a_oListValues[nPivot], a_oListValues[nRight]) > 0)
+			{
+				nTemp = a_oListValues[nRight];
+				a_oListValues[nRight] = a_oListValues[nPivot];
+				a_oListValues[nPivot] = nTemp;
+			}
+
+			S01SortQuick_02(a_oListValues, a_nLeft, nRight - 1, a_oCompare);
+			S01SortQuick_02(a_oListValues, nRight + 1, a_nRight, a_oCompare);
+		}
+
+		/** 값을 정렬한다 */
+		private static void S01SortValues_02(List<int> a_oListValues,
+			Func<int, int, int> a_oCompare)
+		{
+			S01SortQuick_02(a_oListValues, 0, a_oListValues.Count - 1, a_oCompare);
+		}
+
+		/** 값을 출력한다 */
+		private static void S01PrintValues_02(List<int> a_oListValues)
+		{
+			for(int i = 0; i < a_oListValues.Count; ++i)
+			{
+				Console.Write("{0}, ", a_oListValues[i]);
+			}
+
+			Console.WriteLine();
+		}
+	}
+}
